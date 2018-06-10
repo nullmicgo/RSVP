c57e244a8489ac1961b191c70628c2caf0fde235
nullmicgo@gmail.com <nullmicgo@gmail.com>
2018-06-10 22:02:28 +0800

    Removed useless material from the default and Implement Layout Style to the project

--- rsvp/src/App.js
+++ rsvp/src/App.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header>
+        <h1>RSVP</h1>
+        <p>A Metting App</p>
+        <form>
+            <input type="text" value="Sam Liu" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
+      </header>
+      <div className="main">
+        <div>
+          <h2>Invitees</h2>
+          <label>
+            <input type="checkbox" /> Hide those who haven't responded
+          </label>
+        </div>
+        <table className="counter">
+          <tbody>
+            <tr>
+              <td>Attending:</td>
+              <td>2</td>
+            </tr>
+            <tr>
+              <td>Unconfirmed:</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+        <ul>
+          <li className="pending"><span>Jimmy</span></li>
+          <li className="responded"><span>Charlotte</span>
+            <label>
+              <input type="checkbox" checked /> Confirmed
+            </label>
+            <button>edit</button>
+            <button>remove</button>
+          </li>
+          <li className="responded">
+            <span>Jerry</span>
+            <label>
+              <input type="checkbox" checked /> Confirmed
+            </label>
+            <button>edit</button>
+            <button>remove</button>
+          </li>
+          <li>
+            <span>Nick</span>
+            <label>
+              <input type="checkbox" /> Confirmed
+            </label>
+            <button>edit</button>
+            <button>remove</button>
+          </li>
+        </ul>
       </div>
+    </div>
     );
   }
 }
